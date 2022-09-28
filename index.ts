c28cb73b7e93832659d9b169390f75a8d51700c3
Samuel Peterson <spete@Samuels-MacBook-Pro.local>
2022-09-28 10:33:18 +0200

    FIX: Moved exports.

--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
 // TODO: This should only export things that I want exposed from this package
 //   as in TS types, MSW mocks, and TRPC.
+
+import { ServerRouter } from './src/trpcRouter';
+
+export * from './src/mocks';
+export type AppRouter = ServerRouter;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import { app } from './app';
-import { ServerRouter } from './trpcRouter';
 
 app.listen(process.env.PORT, () => {
   let message = `🚀 Server ready at: http://localhost::${process.env.PORT}`;
   console.log(message);
 });
-
-export * from './mocks';
-export type AppRouter = ServerRouter;
